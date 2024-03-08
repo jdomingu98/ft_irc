e21beb93a7bd17c351f84d78d20ccee34fd15436
jdomingu98 <jadominguezarcas@gmail.com>
2024-03-08 16:31:16 +0100

    Socket connection template and init Server class

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char **argv)
         std::cerr << INVALID_ARGS << std::endl;
         return EXIT;
     }
+    // Check if the port is valid
+    //Server server = Server(std::atoll(argv[1]), argv[2]);
     
     return SUCCESS;
 }
