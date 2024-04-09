77be95e01528720fd94bf6906becbe787b7b249e
jdomingu98 <jadominguezarcas@gmail.com>
2024-04-09 23:58:43 +0200

    fix: close isNumber function bracket

--- src/libsUtils.cpp
+++ src/libsUtils.cpp
@@ -47,6 +47,7 @@ bool isNumber(const std::string& s) {
     std::string::const_iterator it = s.begin();
     while (it != s.end() && std::isdigit(*it)) ++it;
     return !s.empty() && it == s.end();
+}
 
 /**
  * Joins the vector of strings.
