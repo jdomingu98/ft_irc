6e624ed7b24fbb50e6b847592b0862460bcd615b
jdomingu98 <jadominguezarcas@gmail.com>
2024-04-10 22:39:16 +0200

    add catch clause

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,13 @@ int main(int argc, char **argv) {
         std::string port = std::string(argv[1]);
         std::string password = std::string(argv[2]);
         Server::init(port, password);
-    } catch (ServerException& e) {
+    } catch (ServerException &e) {
         std::cerr << e.what() << std::endl;
         Server::getInstance().closeConnections();
+    } catch (CommandNotFoundException &e) {
+        std::cerr << e.what() << std::endl;
     }
 
+    Server::getInstance().closeConnections();
     return SUCCESS;
 }
