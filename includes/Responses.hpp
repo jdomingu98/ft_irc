2003908c6e4daad28bec7c3600d37abc7d9ef416
jdomingu98 <jadominguezarcas@gmail.com>
2024-05-03 00:01:29 +0200

    fix: end of who message

--- includes/Responses.hpp
+++ includes/Responses.hpp
@@ -19,7 +19,7 @@ class Channel;
 # define RPL_END_OF_NAMES(channel) (channel) + " :End of NAMES list"
 # define RPL_CHANNEL_MODE_IS(channel, mode, modeParams) (channel) + " " + (mode) + " " + (modeParams)
 # define RPL_WHO_REPLY(channel, user, host, nick, userType, realname) (channel) + " " + (user) + " " + (host) + " " + SERVER_NAME + " " + (nick) + "H" + (userType) + " :0 " + (realname)
-# define END_OF_WHO(name) (name) + ":End of WHO list"
+# define END_OF_WHO(name) (name) + " :End of WHO list"
 # define RPL_DOWNLOAD(filename) ":File " + (filename) + " has been downloaded successfully"
 
 # define USER_ID(nickname, username, hostname) ":" + (nickname) + "!" + (username) + "@" + (hostname)
