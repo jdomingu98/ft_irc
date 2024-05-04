55bf5efb543d75516e574e95a251eefc13e4b12e
Alejandro Ruzafa <ruzafa8@gmail.com>
2024-05-04 19:56:27 +0200

    clean comments

--- src/commands/NickCommand.cpp
+++ src/commands/NickCommand.cpp
@@ -19,50 +19,26 @@ NickCommand::NickCommand(const std::string& nickname) : ACommand(false), _nickna
  */
 void NickCommand::execute(int clientFd) {
     Server &server = Server::getInstance();
-    Logger::debug("1");
     if (this->_nickname.empty())
         throw NoNicknameGivenException();
 
     if (this->_nickname.size() > MAX_NICKNAME_SIZE || !isValidNickname())
         throw ErroneousNicknameException(this->_nickname);
-    Logger::debug("2");
     User *user = server.getUserByFd(clientFd);
     std::string nickname = user->getNickname();
-    Logger::debug("3");
     if (server.isNicknameInUse(this->_nickname) && nickname != this->_nickname)
         throw NicknameInUseException(this->_nickname);
     
     std::set<User *> users;
     const std::vector<Channel *> channels = user->getChannels();
-    /*std::vector<Channel> cs = server.getChannels();
-    for (size_t i = 0; i < cs.size(); i++)
-    {
-        // Print channel users, please:
-        std::vector<User *> allUsers = cs[i].getAllUsers();
-        std::cout << "Channel " << cs[i].getName() << " has " << allUsers.size() << " users" << std::endl;
-        for (size_t j = 0; j < allUsers.size(); j++)
-            Logger::debug(allUsers[j]->getNickname());
-    }*/
-    Logger::debug("4");
     if (user->isRegistered() && nickname != this->_nickname) {
-        Logger::debug("5");
         users.insert(user);
         for (size_t i = 0; i < channels.size(); i++) {
-            Logger::debug("6");
             const std::vector<User *> allUsers = channels[i]->getAllUsers();
-            std::cout << "Channel " << channels[i]->getName() << " has " << allUsers.size() << " users" << std::endl;
             for (size_t j = 0; j < allUsers.size(); j++)
                 users.insert(allUsers[j]);
-                Logger::debug("8");
         }
-        Logger::debug("9");
         std::set<User *>::const_iterator it;
-        std::cout << "Sending NICK message to " << users.size() << " users" << std::endl;
-        // Print user names:
-        Logger::debug("-----------------------");
-        for (it = users.begin(); it != users.end(); it++)
-            std::cout << (*it)->getNickname() << ", ";
-        std::cout << std::endl;
         for (it = users.begin(); it != users.end(); it++)
             server.sendMessage((*it)->getFd(), 
                         CMD_MSG(nickname, user->getUsername(), user->getHostname(),
