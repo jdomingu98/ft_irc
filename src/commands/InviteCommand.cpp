882cd520fbfa3000a5f32c0d225d8d2b9a206beb
jdomingu98 <jadominguezarcas@gmail.com>
2024-05-07 23:58:00 +0200

    fix(invite): validation added to the correct user

--- src/commands/InviteCommand.cpp
+++ src/commands/InviteCommand.cpp
@@ -34,8 +34,9 @@ void InviteCommand::execute(int clientFd) {
         throw NotOnChannelException(_channelName);
 
     Channel *channel = server.getChannelByName(_channelName);
+    const User *invited = server.getUserByNickname(_nickname);
 
-    if (me->isOnChannel(_channelName))
+    if (invited->isOnChannel(_channelName))
         throw UserOnChannelException(_nickname, _channelName);
 
     if (channel->isInviteOnly() && !channel->isOper(me->getNickname()))
